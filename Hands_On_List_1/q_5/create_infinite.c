de7aa4ec900d53edad58ea6a0a5e941c02d9c35c
prashant979 <93033761+prashant979@users.noreply.github.com>
2026-09-26 09:37:42 +0530

    Update create_infinite.c
    Updated Loop

--- Hands_On_List_1/q_5/create_infinite.c
+++ Hands_On_List_1/q_5/create_infinite.c
@@ -6,15 +6,17 @@ int main() {
 	char filename[50];
 	int fd;
 	sleep(30);
-	for (int i = 0; i < 5; i++) {
-		sprintf(filename,"file_%d.txt",i);
-		fd = creat(filename, 0644);
-		if (fd == -1) {
-			perror("creation unsuccessfull");
-		} else {
-			printf("File Descriptor =%d\n", fd);
+	while(1){
+		for (int i = 0; i < 5; i++) {
+			sprintf(filename,"file_%d.txt",i);
+			fd = creat(filename, 0644);
+			if (fd == -1) {
+				perror("creation unsuccessfull");
+			} else {
+				printf("File Descriptor =%d\n", fd);
+			}
 		}
+		sleep(10);
 	}
-	while(1){}
 	return 0;
 }
